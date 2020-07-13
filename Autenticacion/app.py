87c7b5b9b2e681f3696e29364640f10fe4bea175
miguelsanabria-cualtos <miguel.sanabria@academicos.udg.mx>
2020-07-13 18:58:34 -0500

    llaves en el main

--- Autenticacion/app.py
+++ Autenticacion/app.py
@@ -25,5 +25,5 @@ def logout():
     return home()
 
 if __name__ == "__main__":
-app.secret_key = os.urandom(12)
-app.run(debug=True,host='0.0.0.0', port=5001)
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=5001)
