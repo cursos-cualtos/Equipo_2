15d763e02e29bdc256dcb9df09c69e236f3dbdc7
NegroDorado <elnegrodorado5@gmail.com>
2020-07-15 15:11:54 -0500

    Aplicados cambios de la db_utils

--- Messages/db_utils.py
+++ Messages/db_utils.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 client = MongoClient('localhost', 27017)
 db = client['microservices-course']
@@ -13,7 +14,7 @@ def get_all_messages():
     return list(cursor)
 
 def get_message(id):
-    result = collection.find_one({'_id': id})
+    result = collection.find_one({'_id': ObjectId(id)})
     return result
 
 def add_message(data):
@@ -21,6 +22,6 @@ def add_message(data):
     return id
 
 def edit_message(id, data):
-    result = collection.update_one({'_id': id}, 
+    result = collection.update_one({'_id': ObjectId(id)}, 
         {'$set': data})
     return str(result.modified_count)
